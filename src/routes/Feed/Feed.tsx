d2e3d65e9dabdf2e1bec25e86254cb79b9078efd
emrah sonmez <emrahsonm3z@gmail.com>
2019-10-09 02:47:31 +0300

    auto-deploy test.

--- src/routes/Feed/Feed.tsx
+++ src/routes/Feed/Feed.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Feed: FunctionComponent = () => (
   <div>
-    <h1>Feed</h1>
+    <h1>Feed 333</h1>
 
     <Link to="/">Go to home</Link>
   </div>
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Home: FunctionComponent = () => (
   <div>
-    <h1>Home page</h1>
+    <h1>Home page 2222</h1>
 
     <Link to="/feed">Go to feed</Link>
   </div>
