4fd1c89c34e1f8b8e17632cb8c57884a089f63cb
emrah sonmez <emrahsonm3z@gmail.com>
2019-10-09 01:12:22 +0300

    Setting up Jest with enzyme test environment.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,23 +1,9 @@
 import React from "react";
 import { shallow } from "enzyme";
-
 import App from "./App";
-import Routes from "./routes";
 
-it("has a header", () => {
+it("runs correctly", () => {
   const wrapper = shallow(<App />);
 
-  expect(wrapper.find("header").exists()).toBe(true);
-});
-
-it("has a footer", () => {
-  const wrapper = shallow(<App />);
-
-  expect(wrapper.find("footer").exists()).toBe(true);
-});
-
-it("contains routes", () => {
-  const wrapper = shallow(<App />);
-
-  expect(wrapper.containsMatchingElement(<Routes />)).toBe(true);
+  expect(wrapper).toMatchSnapshot();
 });
